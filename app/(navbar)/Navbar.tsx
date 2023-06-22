0c784b2f60d6bb51715ea17b234a3bb1730e86b3
Martin Maldonado <martinemanuel93@gmail.com>
2023-06-22 20:39:55 -0300

    add navbar base

--- app/(navbar)/Navbar.tsx
+++ app/(navbar)/Navbar.tsx
@@ -1,5 +1,25 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import style from "./Navbar.module.scss";
+
 export default function Navbar() {
-  return (
-	<div>Navbar</div>
-  )
+	const ref = useRef<HTMLHeadingElement>(null);
+	const [isScrolling, setIsScrolling] = useState(false);
+
+	return (
+		<header ref={ref} className={`${style.header}`}>
+			<div className={style.header__menu}>
+				<button className={style.header__menu_icon}>Menu</button>
+				<button className={style.header__menu_title}>
+					3 years of Nft studios
+				</button>
+			</div>
+			<div className={style.header__logo_center}>NFT STUDIOS</div>
+			<div className={style.header__wallet}>conect your wallet</div>
+		</header>
+	);
+}
+
+function ModalMenu() {
+	return <div></div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
 import styles from "./page.module.scss";
 import CanvasGallery from "./(three)/CanvasGallery";
+import Navbar from "./(navbar)/Navbar";
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
+			<Navbar />
 			<CanvasGallery />
-			{/* <div
-				id="container"
-				style={{
-					position: "absolute",
-					height: "100%",
-					width: "100%",
-					overflow: "auto scroll",
-				}}
-			></div> */}
 		</main>
 	);
 }
