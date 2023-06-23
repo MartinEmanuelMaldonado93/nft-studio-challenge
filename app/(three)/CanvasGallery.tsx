86f1daa91cf5e8e43dc5d2ee963cb6969c52e8f2
Martin Maldonado <martinemanuel93@gmail.com>
2023-06-23 16:05:13 -0300

    add fog and loader

--- app/(three)/CanvasGallery.tsx
+++ app/(three)/CanvasGallery.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { OrbitControls, PerformanceMonitor, Scroll, ScrollControls } from "@react-three/drei";
+import { Loader, OrbitControls, PerformanceMonitor, Scroll, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import { Mesh } from "three";
@@ -13,22 +13,26 @@ export default function CanvasGallery() {
 	const [enabled, enable] = useState(true);
 
 	return (
-		<Canvas
-			ref={canvasRef}
-			className={style.canvas}
-			camera={{ far: 500, fov: 80, position: [0, 0.5, 4] }}
-			gl={{antialias: true}}
-		>
-			<ambientLight intensity={0.1} />
-			<ScrollControls infinite>
-				<Scroll>
-					<Corridor />
-					<ContainerImages />
-				</Scroll>
-			</ScrollControls>
-			{/** If PerfMon detects a low framerate it will switch back to harsh shadows */}
-			{/* <PerformanceMonitor onDecline={() => enable(false)} /> */}
-			{/* <OrbitControls /> */}
-		</Canvas>
+		<>
+			<Canvas
+				ref={canvasRef}
+				className={style.canvas}
+				camera={{ far: 500, fov: 80, position: [0, 0.5, 4] }}
+				gl={{ antialias: true }}
+			>
+				<ambientLight intensity={0.01} />
+				<fog attach="fog" args={[0x000000, 1, 9]} />
+				<ScrollControls infinite>
+					<Scroll>
+						<Corridor />
+						<ContainerImages />
+					</Scroll>
+				</ScrollControls>
+				{/** If PerfMon detects a low framerate it will switch back to harsh shadows */}
+				{/* <PerformanceMonitor onDecline={() => enable(false)} /> */}
+				{/* <OrbitControls /> */}
+			</Canvas>
+			<Loader />
+		</>
 	);
 }
